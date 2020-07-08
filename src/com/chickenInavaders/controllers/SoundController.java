06ec7381b23f7973741eefae643553e139e1d57f
guymon92 <guymon92@gmail.com>
2020-07-08 22:09:50 +0300

    improvement SoundController levels and fix zero volume

--- src/com/chickenInavaders/controllers/SoundController.java
+++ src/com/chickenInavaders/controllers/SoundController.java
@@ -6,15 +6,20 @@ import javax.sound.sampled.*;
 import java.io.File;
 
 public class SoundController {
+
+    private static float MIN_VOLUME = -12.0f;
+    private static float INF_VOLUME = -80.0f;
+
+
     public static void play(String fileName) {
         try {
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(fileName));
             Clip clip = AudioSystem.getClip();
             clip.open(audioInputStream);
             FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-            float range = gainControl.getMaximum() - (-12.0f);
-            float gain = Math.abs(range * SettingsController.getInstance().getSoundVolume()/100) + (-12.0f);
-            gainControl.setValue(gain);
+            float range = gainControl.getMaximum() - (MIN_VOLUME);
+            float gain = Math.abs(range * SettingsController.getInstance().getSoundVolume()/100) + (MIN_VOLUME);
+            gainControl.setValue(SettingsController.getInstance().getSoundVolume() == 0 ? INF_VOLUME : gain);
             clip.start();
         } catch (Exception e) {
             if (Commons.IS_DEBUG)
